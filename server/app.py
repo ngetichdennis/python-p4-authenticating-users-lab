067d56c1293b15f16849801fde8b35b42ea5fd24
Dennis <ngetichdennis90@gmail.com>
2024-04-23 09:44:55 +0300

    update of endpoints

--- server/app.py
+++ server/app.py
@@ -47,10 +47,33 @@ class ShowArticle(Resource):
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
+class Login(Resource):
+    def post(self):
+        user=User.query.filter(User.username == request.json['username']).first()
+        session['user_id']=user.id
+
+        # return {'message': 'Logged in'}, 200
+        return user.to_dict()
+class Logout(Resource):
+    def delete(self):
+        session['user_id']=None
+
+        return {'message': 'logged out'}, 204
+class CheckSession(Resource):
+    def get(self):
+        user=User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return user.to_dict()
+        else:
+            return {'message': 'No user logged in'}, 401
+    
 
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+api.add_resource(Login,'/login')
+api.add_resource(Logout,'/logout')
+api.add_resource(CheckSession,'/check_session')
 
 
 if __name__ == '__main__':
